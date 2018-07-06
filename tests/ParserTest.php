89b3c5274c221ea2f6579955bea333e84715edf6
DmitryKartamyshev <dima_kartamyshev@ukr.net>
2018-07-06 09:02:10 +0300

    refactor mockery test

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -4,23 +4,32 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 use App\Parser\Parser;
-
+use Mockery;
 
 class ParserTest extends TestCase
 {
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
     /** @test */
     public function get_meta_information()
     {
         $content = '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+        $service = Mockery::mock(Parser::class);
+        $service->shouldReceive('getMetaInformation')
+            ->andReturn(['X-UA-Compatible' => 'IE=edge']);
         $parser = new Parser((string)$content);
-        $this->assertCount(1, $parser->getMetaInformation());
+        $this->assertEquals(['X-UA-Compatible' => 'IE=edge'], $parser->getMetaInformation());
     }
 
     /** @test */
     public function get_tag_content()
     {
         $content = '<ul>' . PHP_EOL . '<li>Laravel</li> ' . PHP_EOL . '<li>Zend</li> ' . PHP_EOL . '</ul>';
+        $service = Mockery::mock(Parser::class);
+        $service->shouldReceive('getTagContent(\'li\')')
+            ->andReturn(['Laravel', 'Zend']);
         $parser = new Parser((string)$content);
-        $this->assertCount(2, $parser->getTagContent('li'));
+        $this->assertEquals(['Laravel', 'Zend'], $parser->getTagContent('li'));
     }
+
 }
